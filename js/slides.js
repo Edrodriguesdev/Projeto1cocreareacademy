96f04bdea7a63052fabf439c68c6225fa6297aa2
Sidney Alves <sidney.kns@gmail.com>
2020-08-03 20:41:26 -0300

    estilizando plugin slick e colocando dinamica no slider de imagens

--- js/slides.js
+++ js/slides.js
@@ -1,9 +1,26 @@
 $(function(){
-	$('').slick({
-		centerMode:false,
-		slideToShow:5,
-		arrows: false,
-		dots:false,
+	$('.flexImg').slick({
+		arrows: true,
+		dots:true,
+  		infinite: true,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll:5,
+			responsive:[
+				{
+					breakpoint: 768,
+					settings: {
+					arrows: false,
+					dots:true,
+					infinite: true,
+					speed: 300,
+					slidesToShow: 3,
+					slidesToScroll:3,
+				}
+
+				}
+			]
+  	
 	});
 	
 	
