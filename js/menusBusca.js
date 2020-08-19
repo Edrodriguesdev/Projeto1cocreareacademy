c7413f733d16aa5c68316f1cbc64f024dc575d57
Bambalas <57500699+Bambalas@users.noreply.github.com>
2020-08-19 18:20:48 -0300

    Validação preenchimento obrigatorio
    Menus busca

--- js/menusBusca.js
+++ js/menusBusca.js
@@ -14,5 +14,17 @@ var substMenu = function() {
     }
 };
 
+function validaForm(frm) {
+
+    if (frm.hashTag.value == “”” || frm.hashTag.value == null || frm.hashTag.value.lenght < 3) {
+        //É mostrado um alerta, caso o campo esteja vazio.
+        alert(“Por favor, indique o seu nome.“);
+       //Foi definido um focus no campo.
+        frm.nome.focus();
+       //o form não é enviado.
+        return false;
+    }
+    
+  
 window.addEventListener("scroll", substMenu);
 /* Fim da função de scroll do header transparente*/
