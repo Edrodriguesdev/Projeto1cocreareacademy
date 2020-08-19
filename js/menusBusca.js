979df72066b3e7860161f075244b69e82f3287de
Default-nick <67932981+Default-nick@users.noreply.github.com>
2020-08-19 20:45:32 -0300

    Atualização js

--- js/menusBusca.js
+++ js/menusBusca.js
@@ -13,14 +13,16 @@ var substMenu = function() {
         }
     }
 };
+window.addEventListener("scroll", substMenu);
 
+/* Fim da função do scroll do header transparente*/
+
+/* Início validação do campo de busca */
 function validaForm(frm) {
 
-    if ((frm.charCounter.value == “””) (frm.charCounter.value == null) (frm.charCounter.value.lenght < 3) (frm.form-control.value.lenght > 140)) {
-        alert(“Por favor, indique uma hashtag valida.“);
+    if ((frm.charCounter.value == "") (frm.charCounter.value == null) (frm.charCounter.value.lenght < 3) (frm.form-control.value.lenght > 140)) {
+        alert("Por favor, indique uma hashtag válida.");
         return false;
     }
-    
-  
-window.addEventListener("scroll", substMenu);
-/* Fim da função de scroll do header transparente*/
+}
+/* Fim validação do campo de busca */
